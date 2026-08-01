f8cd2f9b776eda8c02c71028b8ac8f4ff1199d68
Your Name <you@example.com>
2026-08-01 18:27:01 +0530

    Support GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/portfolio-foundation/',
+  base: mode === 'github' ? '/portfolio-foundation/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
